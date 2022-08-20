3fa1d20235a144ed71162dff76d6f61fc84f97df
bribrie <ibriebriee@gmail.com>
2022-08-20 15:38:56 +0900

    test: MSW setting

--- shop-test/src/setupTests.js
+++ shop-test/src/setupTests.js
@@ -2,4 +2,15 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import "@testing-library/jest-dom";
+
+import { server } from "./mocks/server";
+
+//모든 테스트 전 서버 실행
+beforeAll(() => server.listen());
+
+//하나하나의 테스트가 끝난 후 리셋
+afterEach(() => server.resetHandlers());
+
+//테스트 끝나면 서버 종료
+afterAll(() => server.close());
